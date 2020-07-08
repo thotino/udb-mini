f035e35f6531838b77cebc677c6c786de248edea
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2020-07-08 16:31:05 +0200

    INIT : first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## WHAT
+All the models used for the MiNi project.
+
+## INSTALL
+### HTTPS
+```sh
+git clone https://github.com/thotino/udb-mini.git
+```
+
+### SSH
+```sh
+git clone git@github.com:thotino/udb-mini.git
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+/* copyright (c) 2011-present CybeleTech - All rights reserved. */
+
+/**
+ * project JSDoc description
+ * @module {Object} module name
+ * @version 1.0.0
+ * @author author name
+ * @requires dependency 1
+ * @requires dependency 2
+ * ...
+ */
+
+"use strict";
+
+//================================================================================
+// dependencies
+//================================================================================
+/** import dependencies here */
+
+//================================================================================
+// config
+//================================================================================
+/** import here configurations */
+
+//================================================================================
+// aliases
+//================================================================================
+/** declare here local variables aliasing some of often used imports / conf options */
+
+//================================================================================
+// module
+//================================================================================
+/** module own code, usually just a list of exports if in index.js */
