5b9e8fcc82442868740a8bb3e41b7c4412ee9694
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2020-07-13 17:45:28 +0200

    doc: JSDoc completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * project JSDoc description
- * @module {Object} module name
+ * @module {Object} index
  * @version 1.0.0
- * @author author name
- * @requires dependency 1
- * @requires dependency 2
- * ...
+ * @author Thotino GOBIN-GANSOU
+ * @requires ./lib/mini-schema
+ * @requires ./lib/order-schema
+ * @requires ./lib/user-schema
  */
 
 "use strict";
@@ -20,18 +20,16 @@ const userModel = require("./lib/user-schema");
 //================================================================================
 // config
 //================================================================================
-/** import here configurations */
 
 //================================================================================
 // aliases
 //================================================================================
-/** declare here local variables aliasing some of often used imports / conf options */
 
 //================================================================================
 // module
 //================================================================================
 module.exports = {
-    miniModel,
-    orderModel,
-    userModel,
-}
+  miniModel,
+  orderModel,
+  userModel,
+};
